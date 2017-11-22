be9750562f3b3727162a53a3d906df9371252144
ITBANK403-06 <ITBANK403-06@ITBANK403-06-PC>
2017-11-22 14:30:02 +0900

    listPlan

--- Project/src/plan/dao/PlanDAOImpl.java
+++ Project/src/plan/dao/PlanDAOImpl.java
@@ -47,6 +47,7 @@ public class PlanDAOImpl implements PlanDAO {
 		
 		for(int i=0;i<sdto.getImgName().size();i++) {
 			sdto.getTargets().get(i).setImg(sdto.getImgName().get(i));
+			sdto.getTargets().get(i).setPath(sdto.getImgPath().get(i));
 			values = new Object[] { 
 					sdto.getTargets().get(i).getSubject(),//not null
 					sdto.getTargets().get(i).getImg(),
