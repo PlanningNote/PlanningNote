690996e71964c93b9f3f60e64d242a36ffe7b626
지현 <wlgus0086@naver.com>
2017-11-29 15:19:27 +0900

    asdasd

--- Project/src/ask/controller/AskController.java
+++ Project/src/ask/controller/AskController.java
@@ -1,9 +1,12 @@
 package ask.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.swing.JOptionPane;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import ask.dao.AskDAO;
@@ -65,11 +70,45 @@ public class AskController {
 		//이제 arg2로 dto 한번에 값 못받아온다.		
 		if(result.hasErrors()) { //에러가 발생하는 이유 중 하나가 String으로 받아왔는데 null값이 들어왔는데 그값을 int형으로 자동형변형 시키면서 오류가 발생한다.
 			dto.setNo(0);
+
+			//파일받기
+		MultipartHttpServletRequest mr = (MultipartHttpServletRequest)arg0;
 		
-			
+		//형변환 되려면 꼭 xml에 MultipartResolver 넣어야 됨.
+		MultipartFile mf = mr.getFile("filename");
+		
+		
+		//파일 제대로왔는지 확인
+		String filename= mf.getOriginalFilename(); //실제 파일이름 올라와짐
+		
+		if(filename==null || filename.trim().equals(""))return null; //파일업로드가안되는거 
+		
+		//파일이 받아졌다면 경로지정 session? request ? 
+		
+		HttpSession session = arg0.getSession();
+		String upPath = session.getServletContext().getRealPath("/imgfile"); //파일즈라는 폴더를 하나만들겠다.
+		
+		
+		//서버에 파일을 옮겨 적기 . (파일쓰기)
+		File file = new File(upPath, filename);
+		
+		try{
+			mf.transferTo(file); //실제 파일 전송
+			System.out.println("파일전송 성공! ");
+		}catch(IOException e) {
+			System.out.println("파일전송실패ㅠㅠ ");
+			e.printStackTrace();
+		}
+		
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("filename",filename);	
+		mav.setViewName("ask_list.jsp");		
+		return mav;
 			
 		}
-		 askDAO.insertAsk(dto);
+		
+	    askDAO.insertAsk(dto);
 		return new ModelAndView("redirect:ask_list.do");
 	}
 	
@@ -154,4 +193,6 @@ public class AskController {
 		return mav;
 	}
 
+	
 }
+
